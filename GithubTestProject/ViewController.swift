f9e14a392f0f33e88e3a2db8820c39b0ffd9bec6
Yogesh Patel <yogeshp614@gmail.com>
2022-02-24 10:04:23 +0530

    Initial commit

---
+++ GithubTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubTestProject
+//
+//  Created by Yogesh Patel on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+
+
+}
+
